9186c2f342b06c22902ba77a2467dd2a8f1cc9b3
Marcos Mello <65691346+MarkWILD1@users.noreply.github.com>
2024-02-15 10:06:01 -0300

    Create user model

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
   return (
     <Navbar className='border-b-2'>
       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Wild's</span>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>TuVidaVale👍</span>
         Blog
       </Link>
       <form>
@@ -36,13 +36,13 @@ export default function Header() {
         </div>
         <Navbar.Collapse>
             <Navbar.Link active={path === "/"} as={'div'}>
-              <Link to='/'>Home</Link>
+              <Link to='/'>Inicio</Link>
             </Navbar.Link>
             <Navbar.Link active={path === "/about"} as={'div'}>
-              <Link to='/about'>About</Link>
+              <Link to='/about'>Sobre Nosotros</Link>
             </Navbar.Link>
             <Navbar.Link active={path === "/projects"} as={'div'}>
-              <Link to='/projects'>Projects</Link>
+              <Link to='/projects'>Proyectos</Link>
             </Navbar.Link> 
           </Navbar.Collapse>
     </Navbar>
